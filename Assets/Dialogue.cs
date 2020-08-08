68eab3ae58996111628af4c4a028e759f07b87e0
Quỳnh Châu <lvqchau@apcs.vn>
2020-08-09 01:31:09 +0700

    intro

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+// Pass into Dialogue when we want new Dialogue
+public class Dialogue {
+    public string name; //name of npc
+
+    [TextArea(3, 10)]
+    public string[] sentences;
+}
---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    private Queue<string> sentences;
+
+    private string GetCurrentScene() {
+        Scene currentScene = SceneManager.GetActiveScene();
+        return currentScene.name; //Intro, Menu
+    }
+
+    void Start() {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue) {
+        if (dialogue.name != "none")
+            nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue() {
+        Debug.Log("end dialogue");
+    }
+}
