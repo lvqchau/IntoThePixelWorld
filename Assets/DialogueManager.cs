e3fe82ecd176aa91b6a94404e2447364e4dd434a
Quỳnh Châu <lvqchau@apcs.vn>
2020-08-09 19:11:52 +0700

    Init scene 1 bg

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -11,6 +11,7 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI dialogueText;
     private Queue<string> sentences;
+    // private Queue<GameObject> videos;
 
     private string GetCurrentScene() {
         Scene currentScene = SceneManager.GetActiveScene();
@@ -19,28 +20,55 @@ public class DialogueManager : MonoBehaviour
 
     void Start() {
         sentences = new Queue<string>();
+        // videos = new Queue<GameObject>();
     }
 
     public void StartDialogue(Dialogue dialogue) {
         if (dialogue.name != "none")
             nameText.text = dialogue.name;
         sentences.Clear();
+        // videos.Clear();
+
         foreach (string sentence in dialogue.sentences) {
             sentences.Enqueue(sentence);
         }
-        DisplayNextSentence();
+        // foreach (GameObject video in dialogue.videos) {
+        //     videos.Enqueue(video);
+        // }
+
+        InvokeRepeating("DisplayNextSentence", 1f, 5f);
     }
 
     public void DisplayNextSentence() {
+        string sentence;
         if (sentences.Count == 0) {
             EndDialogue();
+            CancelInvoke("DisplayNextSentence");
             return;
         }
-        string sentence = sentences.Dequeue();
+        // if (GetCurrentScene() == "Intro") {
+        //     showVideo();
+        // }
+        sentence = sentences.Dequeue();
         dialogueText.text = sentence;
     }
 
+    public void showVideo() {
+        // GameObject video = videos.Dequeue();
+        if (sentences.Count == 3) {
+            //show video for 3s
+            Debug.Log("videonormal");
+        }
+        else if (sentences.Count == 2) {
+            //show video light for 5s
+            Debug.Log("videonormalLight");
+        }
+    }
+
     void EndDialogue() {
+        if (GetCurrentScene() == "Intro") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
         Debug.Log("end dialogue");
     }
 }
