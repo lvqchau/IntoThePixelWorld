4a35237f7a601130a1f1c1a5709947c0e712de34
Thanhisme <trieuthanhtr@gmail.com>
2020-08-20 17:51:13 +0700

    Add script for submit password scene3

---
+++ Assets/Scripts/Scene3/CardGameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardGameController : MonoBehaviour
+{
+    private int fileIndex;
+    public SpriteRenderer imageContainer;
+    public Sprite[] fileOptions;
+
+
+    void Start()
+    {
+        fileIndex = 0;
+    }
+
+    void OnMouseDown()
+    {
+        if (gameObject.name == "downbutton")
+        {
+            if (fileIndex == 0)
+            {
+                fileIndex = 3;
+            }
+            else
+            {
+                fileIndex = fileIndex - 1;
+            }
+        }
+        else
+        {
+            fileIndex = (fileIndex + 1) % 4;
+        }
+        Debug.Log(fileIndex);
+        RenderSprite();
+    }
+
+    void RenderSprite()
+    {
+        imageContainer.sprite = fileOptions[fileIndex];
+    }
+}
---
+++ Assets/Scripts/Scene3/SubmitPassword.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmitPassword : MonoBehaviour
+{
+    public GameObject wrongMessage;
+    public SpriteRenderer[] slots;
+
+    void Start()
+    {
+        wrongMessage.SetActive(false);
+    }
+
+    public void CheckPassword()
+    {
+        if (slots[0].sprite.name != "1" || slots[1].sprite.name != "0" || slots[2].sprite.name != "3" || slots[3].sprite.name != "2")
+        {
+            wrongMessage.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("correct");
+        }
+    }
+}
