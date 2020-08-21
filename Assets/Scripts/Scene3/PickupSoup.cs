dfacc55e2d1331ab63a4dee68a5d54addf737cd6
Thanhisme <trieuthanhtr@gmail.com>
2020-08-21 10:50:05 +0700

    Add pick up script and mushroom

--- Assets/Scripts/Scene3/PickupSoup.cs
+++ Assets/Scripts/Scene3/PickupSoup.cs
@@ -6,5 +6,81 @@ using UnityEngine.EventSystems;
 
 public class PickupSoup : MonoBehaviour
 {
-    
+    private Inventory inventory;
+    public Sprite sp = null;
+    private ShibaControl shibaScript;
+    public GameObject[] NPC;
+    public Texture2D cursorDefault;
+
+    private void Start()
+    {
+        GameObject shiba = GameObject.Find("shiba");
+        inventory = shiba.GetComponent<Inventory>();
+        shibaScript = shiba.GetComponent<ShibaControl>();
+    }
+
+    void OnMouseDown()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            if (gameObject.GetComponent<SpriteRenderer>().sprite.name == "crate")
+            {
+                return;
+            }
+            else
+            {
+                shibaScript.SetTargetPosition();
+                shibaScript.Move();
+                StartCoroutine("WaitForDoneMoving");
+            }
+        }
+    }
+
+    IEnumerator WaitForDoneMoving()
+    {
+        yield return new WaitUntil(() => shibaScript.isMoving == false);
+        if (sp)
+            AddItemToInventory(sp);
+    }
+
+    public void AddItemToInventory(Sprite item)
+    {
+
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.isFull[i] == false)
+            {
+                inventory.isFull[i] = true;
+                if (item == null)
+                {
+                    inventory.slots[i].sprite = sp;
+                }
+                else
+                {
+                    inventory.slots[i].sprite = item;
+                    /*if (item.name == "Yarn Ball")
+                    {
+                        DKatty kattyScript = NPC[0].GetComponent<DKatty>();
+                        kattyScript.setKeyCondition("doneWool");
+                    }*/
+                }
+                Destroy(gameObject);
+                Cursor.SetCursor(cursorDefault, Vector2.zero, CursorMode.Auto);
+                break;
+            }
+        }
+    }
+
+    public void RemoveItemInInventory(string itemString)
+    {
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.slots[i].sprite && inventory.slots[i].sprite.name == itemString)
+            {
+                inventory.isFull[i] = false;
+                inventory.slots[i].sprite = null;
+                break;
+            }
+        }
+    }
 }
--- Assets/Scripts/Scene3/SubmitPassword.cs
+++ Assets/Scripts/Scene3/SubmitPassword.cs
@@ -9,24 +9,31 @@ public class SubmitPassword : MonoBehaviour
     public SpriteRenderer[] slots;
     public GameObject Chest;
     private DFinalChest chestScript;
+    private PickupSoup pickupScript;
 
     void Start()
     {
         wrongMessage.SetActive(false);
         congratMessage.SetActive(false);
         chestScript = Chest.GetComponent<DFinalChest>();
+        GameObject crate = GameObject.Find("crate");
+        pickupScript = crate.GetComponent<PickupSoup>();
     }
 
     public void CheckPassword()
     {
         if (slots[0].sprite.name != "1" || slots[1].sprite.name != "0" || slots[2].sprite.name != "3" || slots[3].sprite.name != "2")
         {
+            congratMessage.SetActive(false);
             wrongMessage.SetActive(true);
         }
         else
         {
+            wrongMessage.SetActive(false);
             congratMessage.SetActive(true);
             chestScript.setCondition("unlocked");
+            Sprite bowls = Resources.Load<Sprite>("Bowls");
+            pickupScript.AddItemToInventory(bowls);
         }
     }
 }
